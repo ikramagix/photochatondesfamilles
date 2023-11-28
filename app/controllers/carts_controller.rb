eddc43ac46abe887817821a3fa1addb5ce0fb885
JerkStanton <sylvain.deligny@yahoo.fr>
2023-11-28 22:16:56 +0100

    Some buttons on the cart show

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,5 +1,5 @@
 class CartsController < ApplicationController
-  before_action :set_cart, only: %i[ show edit update destroy ]
+  before_action :set_cart, only: %i[ edit update destroy ]
 
   # GET /carts or /carts.json
   def index
@@ -8,6 +8,7 @@ class CartsController < ApplicationController
 
   # GET /carts/1 or /carts/1.json
   def show
+    @cart = current_user.cart || current_user.create_cart
   end
 
   # GET /carts/new
