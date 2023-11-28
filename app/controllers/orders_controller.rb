355e01f15286fbb2436236b9ad07446a1c6a96a4
ikramiste <ikrame.saadi@gmail.com>
2023-11-28 16:44:37 +0100

    created initializer for stripe - edited orders controller - added buttons mycart my orders if user signed in

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,31 +1,35 @@
 class OrdersController < ApplicationController
-  before_action :set_order, only: %i[ show edit update destroy ]
+  before_action :set_order, only: %i[show edit update destroy]
+  before_action :set_cart, only: [:new, :create]
 
-  # GET /orders or /orders.json
+  require 'stripe'
+
+  Stripe.api_key = Rails.configuration.stripe[:secret_key]
+
+  YOUR_DOMAIN = 'http://localhost:3000/'
+
   def index
     @orders = Order.all
   end
 
-  # GET /orders/1 or /orders/1.json
   def show
   end
 
-  # GET /orders/new
   def new
     @order = Order.new
+    build_order_items_from_cart
   end
 
-  # GET /orders/1/edit
   def edit
   end
 
-  # POST /orders or /orders.json
   def create
     @order = Order.new(order_params)
 
     respond_to do |format|
       if @order.save
-        format.html { redirect_to order_url(@order), notice: "Order was successfully created." }
+        create_stripe_session
+        format.html { redirect_to stripe_payment_url, notice: "Order was successfully created." }
         format.json { render :show, status: :created, location: @order }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -34,7 +38,6 @@ class OrdersController < ApplicationController
     end
   end
 
-  # PATCH/PUT /orders/1 or /orders/1.json
   def update
     respond_to do |format|
       if @order.update(order_params)
@@ -47,7 +50,6 @@ class OrdersController < ApplicationController
     end
   end
 
-  # DELETE /orders/1 or /orders/1.json
   def destroy
     @order.destroy!
 
@@ -58,13 +60,52 @@ class OrdersController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_order
-      @order = Order.find(params[:id])
+
+  def set_order
+    @order = Order.find(params[:id])
+  end
+
+  def build_order_items_from_cart
+    @cart.cart_items.each do |cart_item|
+      @order.order_items.build(item: cart_item.item)
+    end
+  end
+
+  def create_stripe_session
+    session = Stripe::Checkout::Session.create({
+      payment_method_types: ['card'],
+      line_items: build_line_items_for_stripe,
+      mode: 'payment',
+      success_url: payment_success_order_url(@order),
+      cancel_url: order_url(@order),
+    })
+
+    session_id = session.id
+    session_data = { order_id: @order.id, stripe_session_id: session_id }
+    session[:stripe_session] = session_data
+  end
+
+  def build_line_items_for_stripe
+    line_items = []
+
+    @order.order_items.each do |order_item|
+      line_items << {
+        price_data: {
+          currency: 'eur',
+          product_data: {
+            name: order_item.item.name,
+            description: order_item.item.description,
+          },
+          unit_amount: order_item.item.price * 100,
+        },
+        quantity: 1,
+      }
     end
 
-    # Only allow a list of trusted parameters through.
-    def order_params
-      params.require(:order).permit(:total_price, :user_id)
-    end
+    line_items
+  end
+
+  def order_params
+    params.require(:order).permit(:total_price, :user_id)
+  end
 end
