0bf5bae2aba3298d8a86a77f77ac195756a1ac67
cypradoux <pradouxcyprien@gmail.com>
2023-11-29 14:48:40 +0100

    commit inutile

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,6 +3,7 @@ class Order < ApplicationRecord
   belongs_to :user
 
   before_destroy :destroy_associated_records
+  after_create :clear_cart_after_order
 
   private
 
@@ -16,6 +17,14 @@ class Order < ApplicationRecord
     cart.cart_items.each do |cart_item|
       order.order_items.create(item: cart_item.item)
     end
+    order.update(total_price: cart.total_price)
     order
+    cart.cart_items.destroy_all
+  end
+
+  def clear_cart_after_order
+    cart = Cart.find_by(user_id: self.user_id)
+    cart&.cart_items&.clear
+    cart&.update(total_price: nil)
   end
 end
